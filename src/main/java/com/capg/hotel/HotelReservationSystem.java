e0d9c3d42d82e83c155301cb49937a4693e54f70
jindalakhil <akhiljindal222@gmail.com>
2020-10-15 01:29:37 +0530

    [Akhil]Added: Ability to find cheapest hotel within range of dates

--- src/main/java/com/capg/hotel/HotelReservationSystem.java
+++ src/main/java/com/capg/hotel/HotelReservationSystem.java
@@ -1,10 +1,13 @@
 package com.capg.hotel;
 
+import java.util.Scanner;
+
 import com.capg.dto.*;
 import com.capg.service.impl.*;
 
 public class HotelReservationSystem {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcom to Hotel Reservation System");
 		HotelStructure lakewoodHotel = new HotelStructure("Lakewood", 110);
 		HotelStructure bridgewoodHotel = new HotelStructure("Bridgewood", 160);
@@ -14,5 +17,11 @@ public class HotelReservationSystem {
 		obj.addHotelinList(ridgewoodHotel);
 		obj.addHotelinList(bridgewoodHotel);
 		obj.showHotelList();
+		
+		System.out.println("Enter start date in format dd-MM-yyyy");
+		String start = sc.nextLine();
+		System.out.println("Enter the end date in format dd-MM-yyyy");
+		String end = sc.nextLine();
+		System.out.println("Cheapest Hotel Is: " + obj.findCheapestHotelForGivenRageOfDates(start, end));
 	}
 }
--- src/main/java/com/capg/service/impl/HotelServiceImpl.java
+++ src/main/java/com/capg/service/impl/HotelServiceImpl.java
@@ -1,17 +1,21 @@
 package com.capg.service.impl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import com.capg.dto.*;
 import com.capg.service.HotelService;
 
-public class HotelServiceImpl implements HotelService{
+public class HotelServiceImpl implements HotelService {
 	ArrayList<HotelStructure> hotelList;
-	
+
 	public HotelServiceImpl() {
 		hotelList = new ArrayList<>();
 	}
-	
+
 	public void addHotelinList(HotelStructure hotel) {
 		if (checkHotelExistance(hotel)) {
 			System.out.println("Hotel with this name already exists");
@@ -23,9 +27,43 @@ public class HotelServiceImpl implements HotelService{
 	private boolean checkHotelExistance(HotelStructure hotel) {
 		return hotelList.stream().anyMatch(x -> x.getHotelName().equalsIgnoreCase(hotel.getHotelName()));
 	}
-	
+
 	public void showHotelList() {
 		hotelList.stream().forEach(x -> System.out.println(x));
 	}
-	
+
+	private long numberOfDaysCalculator(Date startDate, Date endDate) {
+		long difference_In_Time = endDate.getTime() - startDate.getTime();
+		return (difference_In_Time / (1000 * 60 * 60 * 24)) % 365;
+	}
+
+	public String findCheapestHotelForGivenRageOfDates(String startDate, String endDate) {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		Date start;
+		Date end;
+	    long min = Long.MAX_VALUE;
+	    String result = null;
+		try {
+			 start = formatter.parse(startDate);
+			 end = formatter.parse(endDate);
+			 System.out.println(start);
+			 System.out.println(end);
+			 long days = numberOfDaysCalculator(start, end);
+			 System.out.println(days);
+			 for(HotelStructure hotel : hotelList ) {
+				long rate = hotel.getHotelRateForRegularCustomers() * days;
+				if(rate < min) {
+					min = rate;
+					result = hotel.getHotelName();
+				}
+			 }
+			 System.out.println("Total rates:" + min);
+			 return result;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
