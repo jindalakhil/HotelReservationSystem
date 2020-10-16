c0c02d285335f066cb69cc73cb7c048a08563812
jindalakhil <akhiljindal222@gmail.com>
2020-10-16 22:54:42 +0530

    [Akhil]Refactor: FindCheapBest Rated Hotel For Reward Customer Meathod Using Streams

--- src/main/java/com/capg/service/impl/HotelServiceImpl.java
+++ src/main/java/com/capg/service/impl/HotelServiceImpl.java
@@ -7,6 +7,8 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.capg.dto.*;
 import com.capg.service.HotelService;
@@ -43,132 +45,140 @@ public class HotelServiceImpl implements HotelService {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 		Date start;
 		Date end;
-	    long min = Long.MAX_VALUE;
-	    String result = null;
+		long min = Long.MAX_VALUE;
+		String result = null;
 		try {
-			 start = formatter.parse(startDate);
-			 end = formatter.parse(endDate);
-			 System.out.println(start);
-			 System.out.println(end);
-			 long days = numberOfDaysCalculator(start, end);
+			start = formatter.parse(startDate);
+			end = formatter.parse(endDate);
+			System.out.println(start);
+			System.out.println(end);
+			long days = numberOfDaysCalculator(start, end);
 //			 System.out.println(days);
-			 for(HotelStructure hotel : hotelList ) {
+			for (HotelStructure hotel : hotelList) {
 				long rate = hotel.getHotelRateForRegularCustomersOnWeekDays() * days;
-				if(rate < min) {
+				if (rate < min) {
 					min = rate;
 					result = hotel.getHotelName();
 				}
-			 }
-			 System.out.println("Total rates:" + min);
-			 return result;
+			}
+			System.out.println("Total rates:" + min);
+			return result;
 		} catch (ParseException e) {
 			System.out.println(e.toString());
 		}
 		return null;
 	}
-	
+
 	private long numberOfWeekendDaysCalculator(Date startDate, Date endDate) {
-		if(startDate.after(endDate)) {
+		if (startDate.after(endDate)) {
 			Date temp = endDate;
 			endDate = startDate;
 			startDate = temp;
 		}
-		long count =0;
-		while(startDate.before(endDate) || startDate.equals(endDate)) {
-			if(startDate.getDay() == 0 || startDate.getDay() == 6) {
+		long count = 0;
+		while (startDate.before(endDate) || startDate.equals(endDate)) {
+			if (startDate.getDay() == 0 || startDate.getDay() == 6) {
 				count++;
 			}
-			Date nextDate  = new Date(startDate.getTime() + (1000 * 60 * 60 * 24));
+			Date nextDate = new Date(startDate.getTime() + (1000 * 60 * 60 * 24));
 			startDate = nextDate;
 		}
 		return count;
 	}
-	
-	public HotelStructure findCheapestHotelForGivenRageOfDatesIncludeWeekendsAndWeekdaysRate(String startDate, String endDate) {
+
+	public HotelStructure findCheapestHotelForGivenRageOfDatesIncludeWeekendsAndWeekdaysRate(String startDate,
+			String endDate) {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 		Date start;
 		Date end;
-	    long min = Long.MAX_VALUE;
+		long min = Long.MAX_VALUE;
 		try {
-			 start = formatter.parse(startDate);
-			 end = formatter.parse(endDate);
-			 System.out.println(start);
-			 System.out.println(end);
-			 long totalDays = numberOfDaysCalculator(start, end);
-			 long weekends = numberOfWeekendDaysCalculator(start, end);
-			 long weekDays = totalDays - weekends;
-			 int rating = 0;
-			 HotelStructure hotelResult = null;
-			 for(HotelStructure hotel : hotelList ) {
-				long rate = (hotel.getHotelRateForRegularCustomersOnWeekDays() * weekDays) + (hotel.getHotelRateForRegularCustomersOnWeekends() * weekends);
-				if(rate < min) {
+			start = formatter.parse(startDate);
+			end = formatter.parse(endDate);
+			System.out.println(start);
+			System.out.println(end);
+			long totalDays = numberOfDaysCalculator(start, end);
+			long weekends = numberOfWeekendDaysCalculator(start, end);
+			long weekDays = totalDays - weekends;
+			int rating = 0;
+			HotelStructure hotelResult = null;
+			for (HotelStructure hotel : hotelList) {
+				long rate = (hotel.getHotelRateForRegularCustomersOnWeekDays() * weekDays)
+						+ (hotel.getHotelRateForRegularCustomersOnWeekends() * weekends);
+				if (rate < min) {
 					min = rate;
 					hotelResult = hotel;
-				} else if(rate == min) {
-					if(rating < hotel.getHotelRating()) {
+				} else if (rate == min) {
+					if (rating < hotel.getHotelRating()) {
 						hotelResult = hotel;
 					}
 				}
-			 }
-			 System.out.println("Total rates:" + min);
-			 return hotelResult;
+			}
+			System.out.println("Total rates:" + min);
+			return hotelResult;
 		} catch (ParseException e) {
 			System.out.println(e.toString());
 		}
 		return null;
 	}
-	
+
 	public HotelStructure findBestRatedHotelForGivenRageOfDates(String startDate, String endDate) {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 		Date start;
 		Date end;
 		try {
-			 start = formatter.parse(startDate);
-			 end = formatter.parse(endDate);
-			 System.out.println(start);
-			 System.out.println(end);
-			 long totalDays = numberOfDaysCalculator(start, end);
-			 long weekends = numberOfWeekendDaysCalculator(start, end);
-			 long weekDays = totalDays - weekends;
-			 int rating = 0;
-			HotelStructure result = hotelList.stream().sorted(Comparator.comparing(HotelStructure::getHotelRating).reversed()).findFirst().orElse(null);
-			long rate = (result.getHotelRateForRegularCustomersOnWeekDays() * weekDays) + (result.getHotelRateForRegularCustomersOnWeekends() * weekends);
-			 System.out.println("Total rates:" + rate);
-			 return result;
+			start = formatter.parse(startDate);
+			end = formatter.parse(endDate);
+			System.out.println(start);
+			System.out.println(end);
+			long totalDays = numberOfDaysCalculator(start, end);
+			long weekends = numberOfWeekendDaysCalculator(start, end);
+			long weekDays = totalDays - weekends;
+			int rating = 0;
+			HotelStructure result = hotelList.stream()
+					.sorted(Comparator.comparing(HotelStructure::getHotelRating).reversed()).findFirst().orElse(null);
+			long rate = (result.getHotelRateForRegularCustomersOnWeekDays() * weekDays)
+					+ (result.getHotelRateForRegularCustomersOnWeekends() * weekends);
+			System.out.println("Total rates:" + rate);
+			return result;
 		} catch (ParseException e) {
 			System.out.println(e.toString());
 		}
 		return null;
 	}
-	
-	public HotelStructure findCheapestBestRatedHotelForGivenRageOfDatesForRewardCustomers(String startDate, String endDate) {
+
+	public HotelStructure findCheapestBestRatedHotelForGivenRageOfDatesForRewardCustomers(String startDate,
+			String endDate) {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 		Date start;
 		Date end;
-	    long min = Long.MAX_VALUE;
+		long minRate = Long.MAX_VALUE;
 		try {
-			 start = formatter.parse(startDate);
-			 end = formatter.parse(endDate);
-			 System.out.println(start);
-			 System.out.println(end);
-			 long totalDays = numberOfDaysCalculator(start, end);
-			 long weekends = numberOfWeekendDaysCalculator(start, end);
-			 long weekDays = totalDays - weekends;
-			 int rating = 0;
-			 HotelStructure hotelResult = null;
-			 for(HotelStructure hotel : hotelList ) {
-				long rate = (hotel.getHotelRateForRewardCustomersOnWeekDays() * weekDays) + (hotel.getHotelRateForRewardCustomersOnWeekends() * weekends);
-				if(rate < min) {
-					min = rate;
-					hotelResult = hotel;
-				} else if(rate == min) {
-					if(rating < hotel.getHotelRating()) {
-						hotelResult = hotel;
-					}
-				}
-			 }
-			 System.out.println("Total rates:" + min);
-			 return hotelResult;
+			start = formatter.parse(startDate);
+			end = formatter.parse(endDate);
+			System.out.println(start);
+			System.out.println(end);
+			long totalDays = numberOfDaysCalculator(start, end);
+			long weekends = numberOfWeekendDaysCalculator(start, end);
+			long weekDays = totalDays - weekends;
+			int rating = 0;
+			HotelStructure hotelResult = null;
+
+			minRate = hotelList.stream()
+					.map(n -> n.getHotelRateForRewardCustomersOnWeekDays() * weekDays
+							+ n.getHotelRateForRewardCustomersOnWeekends() * weekDays)
+					.sorted().findFirst().orElse(null);
+			long minStream = minRate;
+
+			List<HotelStructure> minHotelList = hotelList.stream()
+					.filter(n -> (n.getHotelRateForRewardCustomersOnWeekDays() * weekDays
+							+ n.getHotelRateForRewardCustomersOnWeekends() * weekDays) == minStream)
+					.collect(Collectors.toList());
+
+			hotelResult = minHotelList.stream().max(Comparator.comparing(HotelStructure::getHotelRating)).orElse(null);
+
+			System.out.println("Total rates: " + minRate);
+			return hotelResult;
 		} catch (ParseException e) {
 			System.out.println(e.toString());
 		}
